e3c43ba8d9ec35e060af3603ec2289c8c7a3aa70
qudrat <alimov.kolya@gmail.com>
2021-09-08 12:35:36 +0500

    This project is only for educational purpose

---
+++ AlertForm.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Notification
+{
+    /// <summary>
+    /// Interaction logic for AlertForm.xaml
+    /// </summary>
+
+
+
+    public partial class AlertForm : Window
+    {
+        public static int pressNumber = 0;
+        public static int pressPoition = 0;
+
+        public AlertForm()
+        {
+
+
+            InitializeComponent();
+
+            //var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            //this.Left = desktopWorkingArea.Right - this.Width;
+            //this.Top = desktopWorkingArea.Bottom - this.Height;
+
+            SizeChanged += (o, e) =>
+            {
+
+                var r = SystemParameters.WorkArea;
+                Left = r.Right - ActualWidth;
+                Top = (r.Bottom - pressPoition) - ActualHeight;
+            };
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
+            dispatcherTimer.Start();
+
+
+            
+
+
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notification
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static int windowToClose = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AlertForm alertForm = new AlertForm();
+            alertForm.Show();
+            AlertForm.pressPoition += 145;
+            AlertForm.pressNumber+= 1;
+
+            if (AlertForm.pressNumber % 4 == 0)
+            {
+                windowToClose = AlertForm.pressNumber / 4;
+            }
+            if (AlertForm.pressNumber == 4)
+            {
+                AlertForm.pressPoition = 0;
+                AlertForm.pressNumber = 0;
+            }
+            
+        }
+
+      
+    }
+}
